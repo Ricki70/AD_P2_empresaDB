9fb2a907d81431c4e61977139aa7b9ce54e289b4
IreneLM97 <irenelm1997@gmail.com>
2023-10-17 12:30:11 +0200

    Cambios en menú principal

--- src/main/java/view/MenuPrincipal.java
+++ src/main/java/view/MenuPrincipal.java
@@ -18,13 +18,17 @@ public class MenuPrincipal {
 		while (true) {
 			System.out.print(opciones + "\nIntroduce tu eleccion: ");
 			switch (IO.readString().toUpperCase().charAt(0)) {
-			case '1':
-				MenuGestionDepartamentos.mostrarMenu();
-				break;
-			case '2':
-				MenuGestionEmpleados.mostrarMenu();
-				break;
-			default:
+				case '1':
+					MenuGestionDepartamentos.mostrarMenu();
+					break;
+				case '2':
+					MenuGestionEmpleados.mostrarMenu();
+					break;
+				case '3':
+					System.out.println("\nHas salido del menú");
+					return;
+				default:
+					System.out.println("Opción no válida");
 			}
 		}
 
