ada3520e28230b9ecf07ce2312659b9dae71c926
Ricki70 <ricki.agui70@gmail.com>
2023-10-18 12:21:44 +0200

    Modificacion del insert en la interfaz

--- src/main/java/dao/DaoInterface.java
+++ src/main/java/dao/DaoInterface.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.sql.ResultSet;
+
 /**
  * Interfaz con los métodos necesarios para realizar operaciones CRUD sobre la base de datos.
  * @param <T> tipo de objeto genérico (representa Empleado o Departamento)
@@ -7,14 +9,16 @@ package dao;
 public interface DaoInterface<T> {
 	/**
 	 * Método para listar los registros de tabla.
+	 * @return 
 	 */
-	public void listar();
+	public String listar();
 	
 	/**
 	 * Método para insertar un registro en la tabla.
+	 * @param t registro que se quiere insertar
 	 * @return número de filas afectadas
 	 */
-	public int insert();
+	public int insert(T t);
 	
 	/**
 	 * Método para actualizar un registro de la tabla.
